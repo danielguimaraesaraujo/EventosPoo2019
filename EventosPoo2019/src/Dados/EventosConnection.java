03df0a3be96307a7a510edd22a3833f58125badd
CarlosBueno99 <carlosbueno.contato@gmail.com>
2019-12-07 14:35:29 -0300

    atualizei o autor de algumas classes

--- EventosPoo2019/src/Dados/EventosConnection.java
+++ EventosPoo2019/src/Dados/EventosConnection.java
@@ -14,7 +14,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 /**
  *
- * @author dgaraujo
+ * @author cbueno
  */
 public class EventosConnection {
     
--- EventosPoo2019/src/Dados/HomeConnection.java
+++ EventosPoo2019/src/Dados/HomeConnection.java
@@ -5,10 +5,23 @@
  */
 package Dados;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
- * @author dgaraujo
+ * @author cbueno
  */
 public class HomeConnection {
-    
+
+    public static ResultSet selectHome() throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+        String query = "select a.nome , a.datatarefa, a.descricao, b.Materia ,c.Tarefa,d.Status  from tarefas AS a \n" +
+                        "join tipomateria as b on a.id_tipoMateria = b.id\n" +
+                        "join tipotarefa as c on a.id_tipoTarefa = c.id\n" +
+                        "join cadstatus as d on a.id_cadstatus = d.id";
+        PreparedStatement ps = EventosConnection.ConnectionEventosDB().prepareStatement(query);
+        ResultSet rs = ps.executeQuery();
+        return rs;
+    }
 }
